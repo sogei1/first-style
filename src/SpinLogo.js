ed10e3408d75cc980c66c6367132fad0fd1523ff
Carlo Centofanti <carlocentos@gmail.com>
2022-06-09 10:44:01 +0200

    init

---
+++ src/SpinLogo.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import styles from "./spinLogo.module.css"
+
+function SpinLogo() {
+  return (
+    <div className={styles.App}>
+      <header className={styles.Appheader}>
+        <img src={logo} className={styles.Applogo} alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className={styles.Applink}
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default SpinLogo;
--- src/index.js
+++ src/index.js
@@ -3,11 +3,13 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import SpinLogo from './SpinLogo';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
+    <SpinLogo></SpinLogo>
   </React.StrictMode>
 );
 
